22535b8c8cf3915b14f50d2f542f6905b792b628
Jaskaran Singh <jaskaranmain@gmail.com>
2025-10-07 08:45:41 +0530

    added: leetcode solution

---
+++ problems/Basics/Leetcode #9 Palindrome Number.py
@@ -0,0 +1,10 @@
+    if x < 0:
+                return False
+            num = x
+            palindrome_number = 0
+            while num > 0:
+                last_digit = num % 10
+                palindrome_number = (palindrome_number * 10) + last_digit
+                num //= 10
+                
+            return palindrome_number == x
