00356a84bb2b195e23d7f69358e23ee51877b26c
Jaskaran Singh <jaskaranmain@gmail.com>
2025-10-10 05:25:14 +0530

    solve array + two pointer questions

---
+++ problems/Basics/Armstrong Number.py
@@ -0,0 +1,11 @@
+
+
+
+
+
+
+
+# TEST CASES:
+# 153 = true
+# 372 = false
+# 100 = false
---
+++ problems/easy/Length Of Last Word.py
@@ -0,0 +1,15 @@
+s = "Hello World"
+
+# check if string is empty
+# divide whole string into pieces
+# count charactors in last string
+
+if s:
+    return False
+
+div_string = s.split()
+
+last_word = div_string[-1]
+
+print(len(last_word))
+
